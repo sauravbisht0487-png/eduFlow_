a272d515990a94388a190c1303aa12aa3793058a
Chandra Bhanu Bisht <sauravbisht0487@gmail.com>
2026-08-12 16:16:33 +0530

    fix component import casing

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -4,7 +4,7 @@ import Companies from '../../components/student/Companies';
 import CoursesSection from '../../components/student/CoursesSection';
 import TestimonialsSection from '../../components/student/TestimonialsSection';
 import CallToAction from '../../components/student/CallToAction';
-import Footer from '../../components/student/footer';
+import Footer from '../../components/student/Footer';
 
 const Home = () => {
   return (
